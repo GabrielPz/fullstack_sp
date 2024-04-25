4c88a2cf6b2b550d501c1eb2829333aeeb5857e6
Gabriel <gabrielhenriquedps@gmail.com>
2024-04-25 06:39:51 -0300

    fix: build

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,19 +1,73 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
-import { useEffect } from "react";
+import { Inter } from "next/font/google";import { useEffect, useState } from "react";
+import { Box, Modal, useTheme } from "@mui/material";
+import { getStyles } from "@/styles/styles";
+import SearchIcon from '@mui/icons-material/Search';
+import Header from "@/components/Header";
+import UploadCSV from "@/components/UploadCSV";
+import { Data } from "@/types/data";
+import { getData } from "@/services/backendCalls";
+import Cards from "@/components/Cards";
+import SkeletonComponent from "@/components/SkeletonComponent";
+import {Search, SearchIconWrapper, StyledInputBase} from "../types/constants";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const theme = useTheme();
+  const materialStyles = getStyles(theme);
+  const[openModal, setOpenModal] = useState(false);
+  const handleOpenModal = () => setOpenModal(true);
+  const handleCloseModal = () => setOpenModal(false);
+  const[data, setData] = useState<Data[]>([]);
+  const[fetchingData, setFetchingData] = useState(true);
+  const[searchTerm, setSearchTerm] = useState("");
+  const[refreshState, setRefreshState] = useState(false);
+  const handleRefreshState = () => setRefreshState(!refreshState)
+
   useEffect(() => {
-    
-  },[])
+    const fetchData = async () => {
+      setFetchingData(true);
+      const response = await getData();
+      setData(response.data);
+      setFetchingData(false);
+    };
+    fetchData();
+
+  },[refreshState])
 
   return (
-    <>
-
-    </>
+    <Box sx={{
+      width: '100vw',
+      height: '100vh',
+      padding: '5%',
+      backgroundColor: 'white',
+      overflowY: 'auto'
+    }}>
+      <Modal
+        open={openModal}
+        onClose={handleCloseModal}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <UploadCSV handleCloseModal={handleCloseModal} handleRefreshState={handleRefreshState}/>
+      </Modal>
+      <Header onOpenModal={handleOpenModal}/>
+      <Search>
+          <SearchIconWrapper>
+          <SearchIcon sx={{color: 'gray'}}/>
+          </SearchIconWrapper>
+          <StyledInputBase
+          placeholder="Search…"
+          inputProps={{ 'aria-label': 'search' }}
+          onChange={e => setSearchTerm(e.target.value)}
+          />
+      </Search>
+      {!fetchingData && (
+        <Cards data={data} searchTerm={searchTerm} />
+      )}
+      {fetchingData && (
+        <SkeletonComponent/>
+      )}
+    </Box>
   );
 }
